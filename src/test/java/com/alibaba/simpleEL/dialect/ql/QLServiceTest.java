f6b9da79aff056c6c0520ad6519cb5fd77cb6803
wenshao <wenshao@9f61c299-066f-4315-a573-b4affaf9ac39>
2011-04-14 15:33:28 +0000

    支持QL
    git-svn-id: http://code.alibabatech.com/svn/simpleel/trunk@52 9f61c299-066f-4315-a573-b4affaf9ac39

--- src/test/java/com/alibaba/simpleEL/dialect/ql/QLServiceTest.java
+++ src/test/java/com/alibaba/simpleEL/dialect/ql/QLServiceTest.java
@@ -21,22 +21,13 @@ public class QLServiceTest extends TestCase {
 			this.name = name;
 		}
 
-		public int getAge() {
-			return age;
-		}
+		public int getAge() { return age; }
 
-		public void setAge(int age) {
-			this.age = age;
-		}
+		public void setAge(int age) { this.age = age;}
 
-		public String getName() {
-			return name;
-		}
+		public String getName() {return name;}
 
-		public void setName(String name) {
-			this.name = name;
-		}
-
+		public void setName(String name) {this.name = name;}
 	}
 
 	public void test_0 () throws Exception {
