85c24f34d84fe5fa95e58b60892aaa81a60b3ff4
wenshao <wenshao@9f61c299-066f-4315-a573-b4affaf9ac39>
2011-04-15 02:34:06 +0000

    支持QL
    git-svn-id: http://code.alibabatech.com/svn/simpleel/trunk@55 9f61c299-066f-4315-a573-b4affaf9ac39

--- src/main/java/com/alibaba/simpleEL/dialect/ql/ast/QLLimit.java
+++ src/main/java/com/alibaba/simpleEL/dialect/ql/ast/QLLimit.java
@@ -3,16 +3,41 @@ package com.alibaba.simpleEL.dialect.ql.ast;
 import com.alibaba.simpleEL.dialect.ql.visitor.QLAstVisitor;
 
 public class QLLimit extends QLAstNode {
+	private QLExpr offset;
+	private QLExpr limit;
+	
+	public QLLimit() {
+		
+	}
+	
+	public QLLimit(QLExpr offset, QLExpr limit) {
+		this.offset = offset;
+		this.limit = limit;
+	}
+
+	public QLExpr getOffset() {
+		return offset;
+	}
+
+	public void setOffset(QLExpr offset) {
+		this.offset = offset;
+	}
+
+	public QLExpr getLimit() {
+		return limit;
+	}
+
+	public void setLimit(QLExpr limit) {
+		this.limit = limit;
+	}
 
 	@Override
 	protected void accept0(QLAstVisitor visitor) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void output(StringBuffer buf) {
-		// TODO Auto-generated method stub
 		
 	}
 
