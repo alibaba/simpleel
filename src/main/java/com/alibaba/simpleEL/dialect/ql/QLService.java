90a4ac1850052a46b58e812da8170eee238896a7
wenshao <wenshao@9f61c299-066f-4315-a573-b4affaf9ac39>
2011-04-18 11:33:39 +0000

    重构
    git-svn-id: http://code.alibabatech.com/svn/simpleel/trunk@77 9f61c299-066f-4315-a573-b4affaf9ac39

--- src/main/java/com/alibaba/simpleEL/dialect/ql/QLService.java
+++ src/main/java/com/alibaba/simpleEL/dialect/ql/QLService.java
@@ -12,24 +12,40 @@ import com.alibaba.simpleEL.compile.JdkCompiler;
 public class QLService {
 	private JavaSourceCompiler compiler = new JdkCompiler();
 	private QLPreprocessor preprocessor = new QLPreprocessor();
-	
+
+	public JavaSourceCompiler getCompiler() {
+		return compiler;
+	}
+
+	public void setCompiler(JavaSourceCompiler compiler) {
+		this.compiler = compiler;
+	}
+
+	public QLPreprocessor getPreprocessor() {
+		return preprocessor;
+	}
+
+	public void setPreprocessor(QLPreprocessor preprocessor) {
+		this.preprocessor = preprocessor;
+	}
+
 	public <T> void select(Class<T> clazz, Collection<T> srcCollection, Collection<T> destCollection, String ql, Map<String, Object> context) throws Exception {
 		Map<String, Object> compileContext = new HashMap<String, Object>();
 		compileContext.put("class", clazz);
-		
+
 		JavaSource source = preprocessor.handle(compileContext, ql);
-		
+
 		System.out.println(source.getSource());
-		
+
 		Class<? extends Expr> exprClass = compiler.compile(source);
 
 		Expr compiledExpr = exprClass.newInstance();
-		
+
 		Map<String, Object> evalContext = new HashMap<String, Object>();
 		evalContext.put("_src_", srcCollection);
 		evalContext.put("_dest_", destCollection);
 		evalContext.putAll(context);
-		
+
 		compiledExpr.eval(evalContext);
 	}
 }
