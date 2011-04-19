d969d8b6f6fbc9089d99e7886b15d298fa8d4b9c
wenshao <wenshao@9f61c299-066f-4315-a573-b4affaf9ac39>
2011-04-19 06:02:39 +0000

    支持TinyEL
    git-svn-id: http://code.alibabatech.com/svn/simpleel/trunk@97 9f61c299-066f-4315-a573-b4affaf9ac39

---
+++ src/main/java/com/alibaba/simpleEL/dialect/tiny/visitor/TinyELAstVisitorAdapter.java
@@ -0,0 +1,105 @@
+package com.alibaba.simpleEL.dialect.tiny.visitor;
+
+import com.alibaba.simpleEL.dialect.tiny.ast.TinyELAstNode;
+import com.alibaba.simpleEL.dialect.tiny.ast.TinyELBinaryOpExpr;
+import com.alibaba.simpleEL.dialect.tiny.ast.TinyELIdentifierExpr;
+import com.alibaba.simpleEL.dialect.tiny.ast.TinyELMethodInvokeExpr;
+import com.alibaba.simpleEL.dialect.tiny.ast.TinyELNullExpr;
+import com.alibaba.simpleEL.dialect.tiny.ast.TinyELNumberLiteralExpr;
+import com.alibaba.simpleEL.dialect.tiny.ast.TinyELPropertyExpr;
+import com.alibaba.simpleEL.dialect.tiny.ast.TinyELStringExpr;
+import com.alibaba.simpleEL.dialect.tiny.ast.TinyELVariantRefExpr;
+
+public class TinyELAstVisitorAdapter implements TinyELAstVisitor {
+
+	@Override
+	public void postVisit(TinyELAstNode astNode) {
+		
+	}
+
+	@Override
+	public void preVisit(TinyELAstNode astNode) {
+		
+	}
+
+	@Override
+	public void endVisit(TinyELBinaryOpExpr x) {
+		
+	}
+
+	@Override
+	public boolean visit(TinyELBinaryOpExpr x) {
+		return true;
+	}
+
+	@Override
+	public void endVisit(TinyELIdentifierExpr x) {
+		
+	}
+
+	@Override
+	public boolean visit(TinyELIdentifierExpr x) {
+		return true;
+	}
+
+	@Override
+	public void endVisit(TinyELNullExpr x) {
+		
+	}
+
+	@Override
+	public boolean visit(TinyELNullExpr x) {
+		return true;
+	}
+
+	@Override
+	public void endVisit(TinyELPropertyExpr x) {
+		
+	}
+
+	@Override
+	public boolean visit(TinyELPropertyExpr x) {
+		return true;
+	}
+
+	@Override
+	public void endVisit(TinyELMethodInvokeExpr x) {
+		
+	}
+
+	@Override
+	public boolean visit(TinyELMethodInvokeExpr x) {
+		return true;
+	}
+
+	@Override
+	public void endVisit(TinyELNumberLiteralExpr x) {
+		
+	}
+
+	@Override
+	public boolean visit(TinyELNumberLiteralExpr x) {
+		return true;
+	}
+
+	@Override
+	public void endVisit(TinyELStringExpr x) {
+		
+	}
+
+	@Override
+	public boolean visit(TinyELStringExpr x) {
+		return true;
+	}
+
+	@Override
+	public void endVisit(TinyELVariantRefExpr x) {
+		
+	}
+
+	@Override
+	public boolean visit(TinyELVariantRefExpr x) {
+		return true;
+	}
+
+}
