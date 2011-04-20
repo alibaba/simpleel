766c3d5cd2f0c7e1ba1486eaceeb6a1d5082364e
wenshao <wenshao@9f61c299-066f-4315-a573-b4affaf9ac39>
2011-04-20 06:42:29 +0000

    支持TinyEL
    git-svn-id: http://code.alibabatech.com/svn/simpleel/trunk@125 9f61c299-066f-4315-a573-b4affaf9ac39

--- src/main/java/com/alibaba/simpleEL/dialect/tiny/ast/stmt/TinyELIfStatement.java
+++ src/main/java/com/alibaba/simpleEL/dialect/tiny/ast/stmt/TinyELIfStatement.java
@@ -3,27 +3,54 @@ package com.alibaba.simpleEL.dialect.tiny.ast.stmt;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.alibaba.simpleEL.dialect.tiny.ast.TinyELExpr;
 import com.alibaba.simpleEL.dialect.tiny.visitor.TinyELAstVisitor;
 
 public class TinyELIfStatement extends TinyELStatement {
+	private TinyELExpr condition;
 	private final List<TinyELStatement> statementList = new ArrayList<TinyELStatement>();
+	private final List<ElseIf> elseIfList = new ArrayList<ElseIf>();
+	private Else elseItem;
 
 	public TinyELIfStatement() {
 
 	}
 
+	public TinyELExpr getCondition() {
+		return condition;
+	}
+
+	public void setCondition(TinyELExpr condition) {
+		this.condition = condition;
+	}
+
+	public Else getElse() {
+		return this.elseItem;
+	}
+
+	public void setElse(Else value) {
+		this.elseItem = value;
+	}
+
+	public List<ElseIf> getElseIfList() {
+		return this.elseIfList;
+	}
+
 	public List<TinyELStatement> getStatementList() {
 		return this.statementList;
 	}
-	
+
 	@Override
 	protected void accept0(TinyELAstVisitor visitor) {
-		
+
 	}
 
 	@Override
 	public void output(StringBuffer buf) {
-		
+		buf.append("if(");
+		this.condition.output(buf);
+		buf.append(") {\n");
+		buf.append("\n}");
 	}
 
 	public static class ElseIf {
@@ -49,6 +76,5 @@ public class TinyELIfStatement extends TinyELStatement {
 			return this.statementList;
 		}
 	}
-
 
 }
