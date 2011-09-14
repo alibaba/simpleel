f907150382600254f1a635764d1140399139cda6
wenshao <wenshao@9f61c299-066f-4315-a573-b4affaf9ac39>
2011-09-14 21:57:35 +0000

    bvt
    git-svn-id: http://code.alibabatech.com/svn/simpleel/trunk@188 9f61c299-066f-4315-a573-b4affaf9ac39

--- src/test/java/com/alibaba/simpleEL/dialect/ql/bvt/QLExprParserTest.java
+++ src/test/java/com/alibaba/simpleEL/dialect/ql/bvt/QLExprParserTest.java
@@ -8,9 +8,60 @@ import com.alibaba.simpleEL.dialect.ql.ast.QLExpr;
 import com.alibaba.simpleEL.dialect.ql.parser.QLExprParser;
 
 public class QLExprParserTest extends TestCase {
-	public void test_0 () throws Exception {
-		QLExprParser parser = new QLExprParser("age > 30");
-		QLExpr expr = parser.expr();
-		Assert.assertEquals("age > 30", expr.toString());
-	}
+
+    public void test_0() throws Exception {
+        QLExprParser parser = new QLExprParser("age > 30");
+        QLExpr expr = parser.expr();
+        Assert.assertEquals("age > 30", expr.toString());
+    }
+
+    public void test_1() throws Exception {
+        QLExprParser parser = new QLExprParser("age * 30");
+        QLExpr expr = parser.expr();
+        Assert.assertEquals("age * 30", expr.toString());
+    }
+    
+    public void test_2() throws Exception {
+        QLExprParser parser = new QLExprParser("age | 30");
+        QLExpr expr = parser.expr();
+        Assert.assertEquals("age | 30", expr.toString());
+    }
+    
+    public void test_3() throws Exception {
+        QLExprParser parser = new QLExprParser("age & 30");
+        QLExpr expr = parser.expr();
+        Assert.assertEquals("age & 30", expr.toString());
+    }
+    
+    public void test_4() throws Exception {
+        QLExprParser parser = new QLExprParser("age ^ 30");
+        QLExpr expr = parser.expr();
+        Assert.assertEquals("age ^ 30", expr.toString());
+    }
+    
+    public void test_5() throws Exception {
+        QLExprParser parser = new QLExprParser("age = 30");
+        QLExpr expr = parser.expr();
+        Assert.assertEquals("age = 30", expr.toString());
+    }
+    
+    public void test_6() throws Exception {
+        QLExprParser parser = new QLExprParser("a + b - c > 5");
+        QLExpr expr = parser.expr();
+        Assert.assertEquals("a + b - c > 5", expr.toString());
+    }
+    
+    
+    public void test_7() throws Exception {
+        QLExprParser parser = new QLExprParser("age >= 30");
+        QLExpr expr = parser.expr();
+        Assert.assertEquals("age >= 30", expr.toString());
+    }
+    
+    
+    public void test_8() throws Exception {
+        QLExprParser parser = new QLExprParser("a <= b and c >= d");
+        QLExpr expr = parser.expr();
+        Assert.assertEquals("a <= b AND c >= d", expr.toString());
+    }
 }
