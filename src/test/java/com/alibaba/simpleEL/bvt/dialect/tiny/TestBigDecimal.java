7517c3f6ea35c10e7130eada4b56d8802d007385
wenshao <wenshao@9f61c299-066f-4315-a573-b4affaf9ac39>
2011-10-10 13:32:04 +0000

    补充测试
    git-svn-id: http://code.alibabatech.com/svn/simpleel/trunk@204 9f61c299-066f-4315-a573-b4affaf9ac39

---
+++ src/test/java/com/alibaba/simpleEL/bvt/dialect/tiny/TestBigDecimal.java
@@ -0,0 +1,29 @@
+package com.alibaba.simpleEL.bvt.dialect.tiny;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import com.alibaba.simpleEL.dialect.tiny.TinyELEvalService;
+
+public class TestBigDecimal extends TestCase {
+
+    public void test_decimal() throws Exception {
+        TinyELEvalService service = new TinyELEvalService();
+        
+        service.regsiterVariant(BigDecimal.class, "a", "b");
+        
+        Map<String, Object> ctx = new HashMap<String, Object>();
+        ctx.put("a", new BigDecimal("123"));
+        ctx.put("b", new BigDecimal("456"));
+        Assert.assertEquals(Boolean.FALSE, service.eval(ctx, "a > b"));
+        Assert.assertEquals(Boolean.FALSE, service.eval(ctx, "a >= b"));
+        Assert.assertEquals(Boolean.TRUE, service.eval(ctx, "b >= a"));
+        Assert.assertEquals(Boolean.TRUE, service.eval(ctx, "b >= a"));
+    }
+
+    
+}
